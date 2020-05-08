23723a173de73783f7ef77b20599b2d0dcc695cb
Lucas <lucasclira@outlook.com>
2020-05-08 00:24:09 -0300

    Screens settings

--- snake.py
+++ snake.py
@@ -1,6 +1,18 @@
 import pygame
+from pygame.locals import *
 
 '''First Game'''
 
-pygame.init()
+screen_size = (600, 600)
 
+pygame.init()
+screen = pygame.display.set_mode((screen_size))
+pygame.display.set_caption('Game Snake')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    
+    pygame.display.update()

01841db607b43b14784f26b924c78575f5f131d1
Lucas <lucasclira@outlook.com>
2020-05-08 11:55:04 -0300

    defined snake

--- snake.py
+++ snake.py
@@ -9,6 +9,10 @@ pygame.init()
 screen = pygame.display.set_mode((screen_size))
 pygame.display.set_caption('Game Snake')
 
+snake = [(200,200), (210,200), (220,200)]
+snake_skin = pygame.Surface((10,10))
+snake_skin.fill(255,255,255) #White
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
